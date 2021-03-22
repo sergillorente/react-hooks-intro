33c2e6db11de45844c6dd0bf6eafa3820b807632
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-03-22 15:49:29 +0100

    Getting used to useState and now starting with useEffect

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
   incrementCount = () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
   }
 
   render() {
     return (
-      <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>I was clicked {this.state.count} times</button>
+        <h2>Toggle Light</h2>
+        <div
+          style={{
+            width: '50px',
+            height: '50px',
+            background: this.state.isOn ? 'grey' : 'yellow'
+          }}
+          onClick={this.toggleLight}
+        >
+        </div>
+      </>
     );
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
-const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [isOn, setIsOn] = useState(false)
 
-const incrementCount = () => {
-    setCount(count + 1)
-}
+    // useEffect(()=> {
+    //     document.title = `You have clicked ${count} times`
+    // })
 
-return (
-    <button onClick={incrementCount}>I was clicked {count} times</button>
-)
+    const incrementCount = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    const toggleLight = () => {
+        setIsOn(prevIsOn => !prevIsOn)
+    }
+
+    return (
+        <>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>I was clicked {count} times</button>
+            <h2>Toggle Light</h2>
+            <div
+                style={{
+                    width: '50px',
+                    height: '50px',
+                    background: isOn ? 'yellow' : 'grey'
+                }}
+                onClick={toggleLight}
+            >
+            </div>
+        </>
+    )
 }
 
 export default App
